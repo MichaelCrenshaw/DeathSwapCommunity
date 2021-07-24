e7c691d4f6b6792e097af6bf61ca7402bca5b438
MichaelCrenshaw <57016022+MichaelCrenshaw@users.noreply.github.com>
2021-07-24 17:58:48 -0600

    init

---
+++ src/main/java/us/midius/deathswapcommunity/DeathSwapCommunity.java
@@ -0,0 +1,17 @@
+package us.midius.deathswapcommunity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathSwapCommunity extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
